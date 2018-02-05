a78f86630e900290ae2236fbc72c2e1cc03a1cc1
Jay Bednar <ironviralmedia@gmail.com>
2018-02-05 22:52:51 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,3 +6,12 @@ def reverse_each_word(str)
   end
   new_arr.join(" ")
 end
+
+def reverse_each_word(str)
+  arr_str = str.split
+  new_arr = []
+  arr_str.collect do |word|
+    new_arr << word.reverse
+  end
+  new_arr.join(" ")
+end
