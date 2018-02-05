c2b2c2fe7d2490e377ba4f86b9a34f0836b93804
Jay Bednar <ironviralmedia@gmail.com>
2018-02-05 22:58:00 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,17 +1,21 @@
-def reverse_each_word(str)
-  arr_str = str.split
-  new_arr = []
-  arr_str.each do |word|
-    new_arr << word.reverse
-  end
-  new_arr.join(" ")
-end
+require pry
+
+# def reverse_each_word(str)
+#   arr_str = str.split
+#   new_arr = []
+#   arr_str.each do |word|
+#     new_arr << word.reverse
+#   end
+#   new_arr.join(" ")
+# end
 
 def reverse_each_word(str)
   arr_str = str.split
   new_arr = []
   arr_str.collect do |word|
     new_arr << word.reverse
+    binding.pry
   end
+
   new_arr.join(" ")
 end
